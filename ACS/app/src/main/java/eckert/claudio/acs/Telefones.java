ff75680947b0a9e6034431b501b9bb1f9491bbfd
Claudio Eckert <claudioeckert@gmail.com>
2018-10-10 10:48:58 -0300

    Criado tela nova para telefones, falta corrigir erros

--- ACS/app/src/main/java/eckert/claudio/acs/Telefones.java
+++ ACS/app/src/main/java/eckert/claudio/acs/Telefones.java
@@ -7,16 +7,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class CadastrarEditarPessoas extends AppCompatActivity {
+public class Telefones extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cadastrar_editar_pessoas);
+        setContentView(R.layout.activity_telefones);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
+        FloatingActionButton fab =  findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
