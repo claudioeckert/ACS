87310f8ceeabea676d41716c9989885eebaff046
Claudio Eckert <claudioeckert@gmail.com>
2018-10-10 17:10:16 -0300

    Tela telefones

--- ACS/app/src/main/java/eckert/claudio/acs/Telefones.java
+++ ACS/app/src/main/java/eckert/claudio/acs/Telefones.java
@@ -6,6 +6,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ListView;
 
 public class Telefones extends AppCompatActivity {
 
@@ -13,7 +14,6 @@ public class Telefones extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_telefones);
-        //Toolbar toolbar = findViewById(R.id.toolbar);
-        //setSupportActionBar(toolbar);
+
     }
 }
