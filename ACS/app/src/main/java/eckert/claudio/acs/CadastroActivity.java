fc6a0265635a20c0826f080bba6d62c20670fd5a
Claudio Eckert <claudioeckert@gmail.com>
2018-11-19 14:27:21 -0200

    ok

--- ACS/app/src/main/java/eckert/claudio/acs/CadastroActivity.java
+++ ACS/app/src/main/java/eckert/claudio/acs/CadastroActivity.java
@@ -1,7 +1,9 @@
 package eckert.claudio.acs;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class CadastroActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class CadastroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+    }
+
+    public void abrirActivityLogin (View v) {
+        startActivity(new Intent(getBaseContext(), Login.class));
     }
 }
